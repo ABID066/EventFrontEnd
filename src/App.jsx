bf00b50c42c719d29c591027b804a85c2c84b1a6
ABID066 <mdabid1152@gmail.com>
2025-05-15 14:51:06 +0600

    vercel

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,8 @@ import RegistrationPage from "./pages/RegistrationPage.jsx";
 import DashboardPage from "./pages/DashboardPage.jsx";
 import { getToken } from "./utilities/helper.js";
 import HomePage from "./pages/HomePage.jsx";
-import EventDetailPage from "./pages/EventDetailPage.jsx"; // You need to create this page
-import EventsCategoryPage from "./pages/EventsCategoryPage.jsx"; // You need to create this page
+//import EventDetailPage from "./pages/EventDetailPage.jsx"; // You need to create this page
+//import EventsCategoryPage from "./pages/EventsCategoryPage.jsx"; // You need to create this page
 
 const ProtectedRoute = ({ children }) => {
     return getToken() ? children : <Navigate to="/login" replace />;
@@ -27,12 +27,12 @@ const App = () => {
                     <Route path="/" element={<ProtectedRoute><HomePage /></ProtectedRoute>} />
                     <Route path="/dashboard" element={<ProtectedRoute><DashboardPage /></ProtectedRoute>} />
 
-                    {/* Event Routes */}
+                    {/* Event Routes
                     <Route path="/events/:id" element={<ProtectedRoute><EventDetailPage /></ProtectedRoute>} />
                     <Route path="/events/category/:category" element={<ProtectedRoute><EventsCategoryPage /></ProtectedRoute>} />
 
-                    {/* Catch-all Route for 404 */}
-                    <Route path="*" element={<Page404 />} />
+                    {/* Catch-all Route for 404
+                    <Route path="*" element={<Page404 />} />*/}
                 </Routes>
             </BrowserRouter>
         </Fragment>
